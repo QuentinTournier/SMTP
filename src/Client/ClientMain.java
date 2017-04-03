63e2c09ddb11aec67bb9a084ba5ef80f6b362a89
QuentinTournier <quentin.tournier2@gmail.com>
2017-04-03 10:26:47 +0200

    init

---
+++ src/Client/ClientMain.java
@@ -0,0 +1,7 @@
+package Client;
+
+/**
+ * Created by Quentin on 03/04/2017.
+ */
+public class ClientMain {
+}
---
+++ src/Serveur/ServeurMain.java
@@ -0,0 +1,7 @@
+package Serveur;
+
+/**
+ * Created by Quentin on 03/04/2017.
+ */
+public class ServeurMain {
+}
